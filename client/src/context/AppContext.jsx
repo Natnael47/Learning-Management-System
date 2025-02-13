e7bf036e7febe53e04d419378578da20b60bb12f
Natnael47 <nati47m10@gmail.com>
2025-02-13 20:27:34 +0300

    # courses and sections

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,10 +1,29 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { dummyCourses } from "../assets/assets";
 
 export const AppContext = createContext()
 
 export const AppContextProvider = (props) => {
-    const value = {
 
+    const Currency = import.meta.env.VITE_CURRENCY
+    const navigate = useNavigate();
+
+    const [allCourses, setAllCourses] = useState([]);
+
+    // Set all courses
+    const fetchAllCourses = async () => {
+        setAllCourses(dummyCourses)
+    }
+
+    useEffect(() => {
+        fetchAllCourses();
+    }, [])
+
+    const value = {
+        Currency, navigate,
+        allCourses,
+        fetchAllCourses
     }
     return (
         <AppContext.Provider value={value}>
--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Companies from '../../componentes/student/Companies';
+import CourseSection from '../../componentes/student/CourseSection';
 import Hero from '../../componentes/student/Hero';
 
 const Home = () => {
     return (
         <div className='flex flex-col items-center space-y-7 text-center'>
             <Hero />
+            <Companies />
+            <CourseSection />
         </div>
     )
 }
